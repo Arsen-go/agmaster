382e30adbb013ac3b9ad683888645c9dc6dd6f16
ARSENPC\matev\Desktop <matevosyan2000@gmail.com>
2021-02-01 23:00:16 +0400

    aa

--- server/app.js
+++ server/app.js
@@ -8,25 +8,24 @@ const cons = require("consolidate");
 
 app.use(bodyParser.json());
 app.use(express.static("public"));
-// app.use(express.static("public/images"));
+app.use(express.static("public/images"));
    
-// app.use(express.static("public/assets/js"));
-// app.use(express.static("public/assets/css"));
-// app.use(express.static("public/assets/sass"));
-// app.use(express.static("public/assets/webfonts"));
+app.use(express.static("public/assets/js"));
+app.use(express.static("public/assets/css"));
+app.use(express.static("public/assets/sass"));
+app.use(express.static("public/assets/webfonts"));
 
 app.use(express.static(path.join(__dirname, "node_modules")));
-// app.engine("html", cons.swig);
-// app.set("views" , path.join(__dirname,"..", "public"));
-// app.set("view engine", "html");
-
+app.engine("html", cons.swig);
+app.set("views", path.join(__dirname,"..", "public"));
+app.set("view engine", "html");
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
 	console.log("server listening port-> 3000");  
 });
 
 app.get("/", (req, res) => {
-	res.redirect("/index.html");//(path.join(__dirname, "..", "index.html")); 
+    res.sendFile(path.join(__dirname,"..","/public/indexz.html"));
 });
 
 app.post("/message", (req, res) => {
