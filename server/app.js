b39a91b527d7b91091907336d435f443c30b2958
ARSENPC\matev\Desktop <matevosyan2000@gmail.com>
2021-02-05 22:33:48 +0400

    Update app.js

--- server/app.js
+++ server/app.js
@@ -25,7 +25,8 @@ app.listen(port, () => {
 });
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname,"..","/public/index.html"));
+	res.send("hi");
+    // res.sendFile(path.join(__dirname,"..","/public/index.html"));
 });
 
 app.post("/message", (req, res) => {
