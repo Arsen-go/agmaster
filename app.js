d3920ed0abaeea24d31af4762dc76c77073db483
ARSENPC\matev\Desktop <matevosyan2000@gmail.com>
2021-02-06 13:04:24 +0400

    FIX images

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use("/", indexRouter);
 
 app.use(bodyParser.json());
 
-// app.use(express.static("public"));
+app.use(express.static("public"));
 app.use(express.static("html/images"));
    
 app.use(express.static("html/assets/js"));
